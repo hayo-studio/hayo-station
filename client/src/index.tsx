91fcda71fe53ed70e8383614bd56a2b9964aaf6a
Harrie <varharrie@gmail.com>
2017-03-15 17:34:01 +0800

    import base styles

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import {AppContainer} from 'react-hot-loader'
 import Root from './Root'
+import './styles/base.styl'
 
 const render = (Root: any) => {
   ReactDOM.render(
