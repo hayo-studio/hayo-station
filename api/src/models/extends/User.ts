b336ba1eea70ce368d6d12b4e5f43a1216b817c3
Harrie <varharrie@gmail.com>
2017-03-16 17:40:26 +0800

    fix typo

--- api/src/models/extends/User.ts
+++ api/src/models/extends/User.ts
@@ -2,19 +2,19 @@ import {Schema} from 'mongoose'
 import * as crypto from 'crypto'
 
 export default function (UserSchema: Schema) {
-  UserSchema.static('register', async (username: String, password: String, email: String) => {
+  UserSchema.static('register', async (username: string, password: string, email: string) => {
     let user = await this.findOne({$or: [{username}, {email}]}).exec()
     if (user) throw new Error('用户名或电子邮箱已注册')
 
-    const passwordHash = crypto.createHash('md5').update('passowrd').digest('hex')
+    const passwordHash = crypto.createHash('md5').update(password).digest('hex')
 
     return this.create({user, password: passwordHash, email})
   })
 
-  interface ILoginQuery {username?: String, email?: String, password: String}
+  interface ILoginQuery {username?: string, email?: string, password: string}
 
-  UserSchema.static('login', async (username: String, password: String) => {
-    const passwordHash = crypto.createHash('md5').update('passowrd').digest('hex')
+  UserSchema.static('login', async (username: string, password: string) => {
+    const passwordHash = crypto.createHash('md5').update(password).digest('hex')
     const query: ILoginQuery = {password: passwordHash}
     if (username.includes('@')) query.email = username
     else query.username = username
